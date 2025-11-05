a1a4a86098539f57191160b7716bbf0756a683e3
IliesMahd <neqoo.mah@gmail.com>
2025-11-05 14:31:29 +0100

    init default project from old exercice

---
+++ RestApi/Models/Account.cs
@@ -0,0 +1,23 @@
+﻿namespace RestApi.Models;
+
+public class Account
+{
+    public int Id { get; init; }
+    public Bank Bank { get; set; }
+    public User Owner { get; set; }
+    public string IBAN { get; set; }
+    public decimal Balance { get; set; }
+    
+    public Account() { }
+    
+    public Account(int id, Bank bank, User owner, string iban, decimal balance)
+    {
+        Id = id;
+        Bank = bank;
+        Owner = owner;
+        IBAN = iban;
+        Balance = balance;
+        
+        // TODO : Établir les relations bidirectionnelles pour ajouter le compte à la banque et à l'utilisateur
+    }
+}
---
+++ RestApi/Models/Bank.cs
@@ -0,0 +1,17 @@
+﻿namespace RestApi.Models;
+
+public class Bank
+{
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public List<Account> Accounts { get; set; } = new List<Account>();
+    public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+    
+    public Bank() { }
+    
+    public Bank(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
